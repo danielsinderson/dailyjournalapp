e64b4fb1ca0bd218ab6bb4c7b375e8e33becee2e
Daniel Sinderson <drsinderson@gmail.com>
2023-07-24 09:58:37 -0700

    - Wrote main() - Wrote select_chore() - Added some imports

--- dailyjournalapp/journalapp.py
+++ dailyjournalapp/journalapp.py
@@ -9,8 +9,23 @@ It will do the following:
     6. Print out current state of projects (track progress in json)
 """
 
+import sys
+import datetime
+import json
+import random
+
+
 def select_chore() -> str:
-    pass
+    """
+    Accesses chores.json, loads it, and randomly selects a chore based on weighting.
+    
+    Returns string containing the chore to do that day
+    """
+    with open('chores.json') as chores_file:
+        chores = json.load(chores_file)
+    
+    chores_list = [chore * chores[chore] for chore in chores.keys()]
+    return random.choice(chores_list)
 
 
 def select_flashcards() -> dict:
@@ -21,7 +36,15 @@ def generate_prompt() -> str:
     pass
 
 
-def display_previous_entry() -> str:
+def select_previous_entry() -> str:
+    pass
+
+
+def display_contents(chore: str, 
+                     flashcards: dict, 
+                     prompt: str,
+                     previous_entry: str
+                     ) -> tuple(str, str):
     pass
 
 
@@ -30,7 +53,17 @@ def collect_entry_and_write_to_markdown(poem: str, journal_entry: str) -> None:
 
 
 def main():
-    pass
+    chore: str = select_chore()
+    flashcards: dict = select_flashcards()
+    prompt: str = generate_prompt()
+    previous_entry: str = select_previous_entry()
+    
+    poem, journal: tuple(str, str) = display_contents(chore, 
+                                                      flashcards,
+                                                      prompt,
+                                                      previous_entry)
+    
+    collect_entry_and_write_to_markdown(poem, journal)
 
 
 if __name__ == "__main__":
