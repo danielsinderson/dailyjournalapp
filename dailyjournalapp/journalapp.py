5c72db6d3e1989b9982856a968e59e749dcaf28a
Daniel Sinderson <drsinderson@gmail.com>
2023-07-26 06:37:03 -0700

    Did some refactoring and cleanup

--- dailyjournalapp/journalapp.py
+++ dailyjournalapp/journalapp.py
@@ -15,34 +15,37 @@ import json
 import random
 
 
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+
 def select_chore() -> str:
     """
     Accesses chores.json, loads it, and randomly selects a chore based on weighting.
     
     Returns string containing the chore to do that day
     """
-    script_dir = os.path.dirname(os.path.abspath(__file__))
     with open(script_dir + '/data/chores.json') as chores_file:
         chores: dict = json.load(chores_file)
     
     chores_list = [chore for chore in chores.keys() for i in range(chores[chore])]
-    return random.choice(chores_list)
+    return f'Your chore for the day is to {random.choice(chores_list)}.'
 
 
-def select_workouts() -> list:
+def select_workouts() -> str:
     """
     Accesses daily_workout.json, loads it, and randomly selects a light and regular workout.
     
     Returns string containing the workout options to do that day
     """
-    script_dir = os.path.dirname(os.path.abspath(__file__))
     with open(script_dir + '/data/daily_workout.json') as workouts_file:
         workouts: dict = json.load(workouts_file)
     
-    light_workout = '\n'.join(random.choice(workouts["light workouts"]))
-    workout = '\n'.join(random.choice(workouts["workouts"]))
-    
-    return [light_workout, workout]
+    light_workout = random.choice(workouts["light workouts"])
+    workout = random.choice(workouts["workouts"])
+    workout_options = f'Your light workout option is to do a {light_workout} ' \
+        f'and your regular workout option is to do a {workout}. ' \
+        f'If you feel like going for a heavier workout, just increase the intensity of either one and/or do both.'
+    return workout_options
 
 
 def select_lens() -> str:
@@ -51,23 +54,34 @@ def select_lens() -> str:
     
     Returns string containing the day's reading to use for reflection.
     """
-    script_dir = os.path.dirname(os.path.abspath(__file__))
     with open(script_dir + '/data/procgen_tarot.json') as tarot_file:
         tarot: dict = json.load(tarot_file)
         cards: list = tarot['cards']
     
     card = random.choice(cards)
-    return card
+    lens = f'Your lens for the day is {card["word"]}. {card["interpretation"]}'
+    return lens
 
 
 def select_previous_entry() -> str:
     return ''
 
 
+def show_project_statuses() -> dict:
+    with open(script_dir + '/data/projects_status.json') as project_statuses_file:
+        projects: list = json.load(project_statuses_file)['projects']
+    
+    in_progress = [project for project in projects if project['status'] == 'In Progress']
+        
+    
+    return ''
+
+
 def write_contents_to_markdown(chore: str, 
                                workouts: str, 
                                lens: str,
-                               previous_entry: str
+                               previous_entry: str,
+                               project_statuses: dict
                                ) -> None:
     pass
 
@@ -75,16 +89,19 @@ def write_contents_to_markdown(chore: str,
 def main():
     chore: str = select_chore()
     print(chore)
-    workout_options: list = select_workouts()
+    workout_options: str = select_workouts()
     print(workout_options)
     lens: str = select_lens()
     print(lens)
+    project_statuses: dict = show_project_statuses()
+    print(project_statuses)
     previous_entry: str = select_previous_entry()
     
     write_contents_to_markdown(chore=chore,
                                workouts=workout_options,
                                lens=lens,
-                               previous_entry=previous_entry)
+                               previous_entry=previous_entry,
+                               project_statuses=project_statuses)
 
 
 if __name__ == "__main__":
